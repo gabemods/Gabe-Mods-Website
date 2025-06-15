66183ff3d88ff1101196d61e9d57bc7e5d8c5a73
Jacob Thomas <41988041+bocajthomas@users.noreply.github.com>
2025-06-15 01:24:19 +0100

    feat: theme logic
    - added theme logic (JavaScript & CSS)
    - supports system, light & dark theme

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -3,6 +3,9 @@ const menuButton = menuButtonContainer.querySelector('md-icon');
 const menuContainer = document.querySelector('.menu-container');
 const settingsButton = document.getElementById('settings-button');
 const settingsMenu = document.getElementById('settings-menu');
+const themeSelect = document.getElementById('theme');
+
+document.getElementById('current-year').textContent = new Date().getFullYear();
 
 menuButtonContainer.addEventListener('click', function() {
   if (menuButton.textContent.trim() === 'menu') {
@@ -14,8 +17,41 @@ menuButtonContainer.addEventListener('click', function() {
   }
 });
 
-document.getElementById('current-year').textContent = new Date().getFullYear();
-
 settingsButton.addEventListener('click', async () => {
   await settingsMenu.show();
+});
+
+function applyTheme(theme) {
+  if (theme === 'system') {
+    document.documentElement.removeAttribute('data-theme');
+    const mq = window.matchMedia('(prefers-color-scheme: dark)');
+    if (mq.matches) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
+    mq.onchange = (e) => {
+      document.documentElement.setAttribute('data-theme', e.matches ? 'dark' : 'light');
+    };
+  } else {
+    document.documentElement.setAttribute('data-theme', theme);
+  }
+}
+
+function getPreferredTheme() {
+  let theme = localStorage.getItem('theme');
+  if (!theme) {
+    localStorage.setItem('theme', 'system');
+    theme = 'system';
+  }
+  return theme;
+}
+
+themeSelect.value = getPreferredTheme();
+applyTheme(themeSelect.value);
+
+themeSelect.addEventListener('change', () => {
+  const selectedTheme = themeSelect.value;
+  localStorage.setItem('theme', selectedTheme);
+  applyTheme(selectedTheme);
 });
